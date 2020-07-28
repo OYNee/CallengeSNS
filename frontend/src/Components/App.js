af9f83df1f1f00ba69734ae1251b57c2f0864a7a
오유민 <dbals75@naver.com>
2020-07-28 17:15:15 +0900

    S03P12A508-62 #comment Update GlobalStyles.js

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
 
 export default () => (
-  <>
+  <ThemeProvider theme={Theme}>
     <GlobalStyles />
-    hello
-  </>
+  </ThemeProvider>
 );
--- frontend/src/Styles/Theme.js
+++ frontend/src/Styles/Theme.js
@@ -1,7 +1,19 @@
 // 백그라운드, 텍스트 등 테마 컬러 설정
+const BOX_BORDER = "1px solid #e6e6e6";
+const BORDER_RADIUS = "4px";
+
 export default {
   bgColor: "#FAFAFA",
   blackColor: "#262626",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  redColor: "#ED4956",
   blueColor: "#3897f0",
   darkBlueColor: "#003569",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `${BOX_BORDER};
+             ${BORDER_RADIUS};
+             background-color:white;
+            `,
 };
