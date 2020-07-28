4fa490e5a24edc660cba4776d53341d50d40f2b4
오유민 <dbals75@naver.com>
2020-07-28 17:52:09 +0900

    S03P12A508-66 #comment Update Query

--- frontend/src/Styles/GlobalStyles.js
+++ frontend/src/Styles/GlobalStyles.js
@@ -1,9 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+
 //styled-components의 최신 방식(global styles)
 export default createGlobalStyle`
     ${reset};
-    *{
+    * {
         box-sizing:border-box;
+    }
+    body {
+        background-color:${(props) => props.theme.bgColor};
+        color:${(props) => props.theme.blackColor};
+    }
+    a {
+        color:${(props) => props.theme.blueColor};
+        text-decoration:none;
     }
 `;
