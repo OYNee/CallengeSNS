29aa0891808da41672630fac44f8303d89cad164
Daniel_Lee <rodgerslee92@gmail.com>
2020-07-28 18:55:02 +0900

    S03P12A508-46 #comment 좋아요기능 및 숫자 보여주기

---
+++ frontend/src/Components/Icons.js
@@ -0,0 +1,12 @@
+
+
+export const Logo = ({size = 24}) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="{size}"
+        height="{size}"
+        viewBox="0 0 24 24"
+    >
+
+    </svg>
+);
