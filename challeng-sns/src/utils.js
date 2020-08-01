5d29de1a544a444d422a107bce8eba10dd731a68
오유민 <dbals75@naver.com>
2020-08-02 02:25:10 +0900

    Update utils.js

--- challeng-sns/src/utils.js
+++ challeng-sns/src/utils.js
@@ -1,5 +1,4 @@
-
-import mgTransport from "nodemailer-mailgun-transport"
+import mgTransport from "nodemailer-mailgun-transport";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
 
@@ -7,34 +6,33 @@ import { adjectives, nouns } from "./words";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
-  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+  return `${adjectives[randomNumber]}${nouns[randomNumber]}`;
 };
-console.log( process.env.API_KEY, process.env.DOMAIN)
-const sendMail = email => {
-    const options = {
-      auth: {
-        api_key: process.env.API_KEY,
-        domain: process.env.DOMAIN
-      }
-    };
-    const client = nodemailer.createTransport(mgTransport(options));
-    // console.log(client)
-    return client.sendMail(email);
+console.log(process.env.API_KEY, process.env.DOMAIN);
+const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_key: process.env.API_KEY,
+      domain: process.env.DOMAIN,
+    },
   };
+  const client = nodemailer.createTransport(mgTransport(options));
+  // console.log(client)
+  return client.sendMail(email);
+};
 
+// const mg = mailgun({apiKey: process.env.API_KEY, domain: process.env.DOMAIN});
 
- // const mg = mailgun({apiKey: process.env.API_KEY, domain: process.env.DOMAIN});
-
-  export const sendSecretMail = (address, secret) => {
-    console.log(address)
-    console.log("email:")
-    const email = {
-      from: "admin@challengram.com",
-      to: address,
-      subject: "🔒Login Secret for challengram",
-      html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to log in`
-    };
-    return sendMail(email);
+export const sendSecretMail = (address, secret) => {
+  console.log(address);
+  console.log("email:");
+  const email = {
+    from: "admin@challengram.com",
+    to: address,
+    subject: "🔒Login Secret for challengram",
+    html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to log in`,
   };
+  return sendMail(email);
+};
 
-  export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
+export const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET);
