a0d39723144ca0b36ac33e377cf0881b169d6c43
이원준 <wonjun9090@naver.com>
2020-08-06 18:07:15 +0900

    S03P13A508-26 #comment 게시물 생성 아이콘 추가

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import {HeartEmpty, User, Logo, Home, Search } from "./Icons";
+import {HeartEmpty, User, Logo, Home, Search, VideoIcon, PhotoIcon, AudioIcon, TextIcon } from "./Icons";
 import { useQuery } from "react-apollo-hooks";
 import { ME } from "../SharedQueries";
 import { Link } from "react-router-dom";
+import {  Grid,  Segment,  Sidebar,} from 'semantic-ui-react';
 
 
 const Footer = styled.footer`
@@ -33,10 +34,89 @@ const ListItem = styled.li`
   text-align: center;
 `;
 
+function exampleReducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_ANIMATION':
+      return { ...state, animation: action.animation, visible: !state.visible }
+    case 'CHANGE_DIMMED':
+      return { ...state, dimmed: action.dimmed }
+    case 'CHANGE_DIRECTION':
+      return { ...state, direction: action.direction, visible: false }
+    default:
+      throw new Error()
+  }
+};
+
+const Header = styled.div`
+  display: block;
+  width: 80%;
+  font-size:2rem;
+  padding-bottom: 10px;
+`
+
+const CloseButton = styled.div`
+  background-color:white;
+  width: 20%;
+  border: 0px;
+  font-size: 1.2rem;
+  padding-top: 14px;
+  text-align: right;
+`
+
+const Line = styled.hr`
+  width:100%;
+  margin: 0;
+  border-color: rgba(0,0,0,0.2);
+`
+
 export default () => {
   const { data } = useQuery(ME);
+
+  const [state, dispatch] = React.useReducer(exampleReducer, {
+    animation: 'overlay',
+    direction: 'bottom',
+    visible: false,
+  })
+
+  const { animation, direction, visible } = state
+  const vertical = direction === 'bottom'
+  
   return (
     <Footer>
+        {vertical && (
+          <Sidebar
+          as={Segment}
+          animation={animation}
+          direction={direction}
+          visible={visible}
+        >
+          <Grid textAlign='center'>
+            <Header onClick={() =>
+                  dispatch({ type: 'CHANGE_ANIMATION', animation: 'overlay' })
+                  }>
+              <h1>New Challenge</h1>
+            </Header>
+            <CloseButton onClick={() =>
+              dispatch({ type: 'CHANGE_ANIMATION', animation: 'overlay' })
+              }> X</CloseButton>
+              <Line/>
+            <Grid.Row columns={4}>
+              <Grid.Column>
+                <VideoIcon />
+              </Grid.Column>
+              <Grid.Column>
+                <PhotoIcon />
+              </Grid.Column>
+              <Grid.Column>
+                <AudioIcon />
+              </Grid.Column>
+              <Grid.Column>
+                <TextIcon />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Sidebar>
+        )}
       <List>
         <ListItem>
           <Link to="/">
@@ -49,9 +129,13 @@ export default () => {
           </Link>
         </ListItem>
         <ListItem>
-          <Link to="/explore">
-            <Logo />
-          </Link>
+        <div
+          onClick={() =>
+            dispatch({ type: 'CHANGE_ANIMATION', animation: 'overlay' })
+          }
+        >
+          <Logo/>
+        </div>
         </ListItem>
         <ListItem>
           <Link to="/notifications">
