5a4dca5750c2c9d2d0eb594e9bc21c8d343a1843
오유민 <dbals75@naver.com>
2020-08-06 22:44:53 +0900

    S03P13A508-22 #comment Update delete account

--- challeng-sns/src/api/User/deleteAccount/deleteAccount.js
+++ challeng-sns/src/api/User/deleteAccount/deleteAccount.js
@@ -1,7 +1,7 @@
 import { prisma } from "../../../../generated/prisma-client";
 import bcrypt from "bcryptjs";
 
-export default {
+var _default = {
   Mutation: {
     deleteAccount: async (_, args, { request, isAuthenticated }) => {
       const { passwd } = args;
@@ -10,16 +10,18 @@ export default {
 
       console.log(user.id);
       console.log(passwd);
-      const passwdMatch = bcrypt.compare(passwd, user.passwd);
-      console.log(bcrypt.compare(passwd, user.passwd));
+      const passwdMatch = await bcrypt.compare(passwd, user.passwd);
 
       if (!passwdMatch) {
         throw new Error("비밀번호가 일치하지 않습니다.");
       }
       console.log("asd");
-      return prisma.deleteUser({
-        where: { id: user.id },
+
+      await prisma.deleteUser({
+        id: user.id,
       });
+      return true;
     },
   },
 };
+exports["default"] = _default;
