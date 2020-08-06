8d9bc5de81ecd83534fae807456725339ffec25d
오유민 <dbals75@naver.com>
2020-08-06 14:02:41 +0000

    Update UserSetting.js

--- frontend/src/Components/UserSetting.js
+++ frontend/src/Components/UserSetting.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Dropdown, Button, Modal } from "semantic-ui-react";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // TODO: This is missing functionality for sub-menu here from SUI core examples.
 // The "Publish To Web" item should contain a sub-menu.
