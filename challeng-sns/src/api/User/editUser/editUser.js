1d2438339feec48ade32c8ceff3ceb1074a31679
오유민 <dbals75@naver.com>
2020-08-08 12:40:29 +0000

    Update editUser.js

--- challeng-sns/src/api/User/editUser/editUser.js
+++ challeng-sns/src/api/User/editUser/editUser.js
@@ -2,14 +2,25 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    editUser: (_, args, { request, isAuthenticated }) => {
+    editUser: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
+      console.log(args);
       const { userid, bio, avatar } = args;
       const { user } = request;
-      return prisma.updateUser({
-        where: { id: user.id },
-        data: { userid, bio, avatar },
-      });
+
+      if (userid == "") {
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: { userid: user.username, bio: bio },
+        });
+        return true;
+      } else {
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: { userid: userid, bio: bio },
+        });
+        return true;
+      }
     },
   },
 };
