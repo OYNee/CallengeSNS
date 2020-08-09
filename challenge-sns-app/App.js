87c286aeccc22d7d8d5e245d4b71ed1b5036a1ba
오유민 <dbals75@naver.com>
2020-08-09 21:19:52 +0900

    S03P13A508-56 #comment Update App Setup

--- challenge-sns-app/App.js
+++ challenge-sns-app/App.js
@@ -1,18 +1,50 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { AppLoading } from "expo";
-import { Text, View } from "react-native";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { AsyncStorage } from "react-native";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "react-apollo-hooks";
+import apolloClientOptions from "./apollo";
+import styles from "./styles";
+import NavController from "./components/NavController";
+import { AuthProvider } from "./AuthContext";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   const preLoad = async () => {
+    // 앱에서 필요한 자원들을 미리 불러오기
     try {
+      // 폰트 불러오기
       await Font.loadAsync({
         ...Ionicons.font,
       });
-      await Asset.loadAsync([require("./assets/logo.png")]);
+      // 이미지 자원 배열로 가져오기
+      await Asset.loadAsync([require("./assets/tempLogo.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions,
+      });
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if (!isLoggedIn || isLoggedIn === "false") {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.log(e);
     }
@@ -20,10 +52,15 @@ export default function App() {
   useEffect(() => {
     preLoad();
   }, []);
-  return loaded ? (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+
+  return loaded && client && isLoggedIn !== null ? (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={styles}>
+        <AuthProvider isLoggedIn={isLoggedIn}>
+          <NavController />
+        </AuthProvider>
+      </ThemeProvider>
+    </ApolloProvider>
   ) : (
     <AppLoading />
   );
