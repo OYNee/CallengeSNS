bb4bbf9ee31d691484919f655539effbe6a14a30
이규진 <leegj93@naver.com>
2020-08-15 00:59:24 +0900

    S03P13A508-82  #comment Mutation 완성(Follow 기능 응용) 및 Post.js 수정

--- challeng-sns/src/api/Challenge/nextChallenge/nextChallenge.js
+++ challeng-sns/src/api/Challenge/nextChallenge/nextChallenge.js
@@ -2,15 +2,22 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    preChallenge: async (_, args, { request, isAuthenticated })  =>{
+    nextChallenge: async (_, args, { request, isAuthenticated })  =>{
         isAuthenticated(request);
         const {user} = request;
-        const {nextPost} = args;
-        const post = await prisma.$exists.post({user:{id: user.id}})
+        const {id, pid, category} = args;
+        const post = await prisma.$exists.post({id, user:{id: user.id}})
         if(post){
-            return prisma.updatePost({
-                data:{prePosts:nextPost},
-                where:{id:user.id}
+           
+            await prisma.updatePost({
+                where:{id},
+                data:{prePosts:{
+                    connect:{
+                        id:pid
+                    }
+                },
+                category:category
+            },
             })
         }else{
             throw Error("잘못된 요청입니다")
