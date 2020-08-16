baf00ea367bd7d93e63e2b96920441b8aab94f51
dusdnr2694@naver.com <dusdnr2694@naver.com>
2020-08-16 21:27:28 +0900

    S03P13A508-75 #comment 검색 부분 수정

--- challeng-sns/src/api/Hashtag/HashtagPost/HashtagPost.js
+++ challeng-sns/src/api/Hashtag/HashtagPost/HashtagPost.js
@@ -1,12 +1,14 @@
 import { prisma } from "../../../../generated/prisma-client";
 
 export default {
-    Query: {
-        followingUser: async (_, args) =>
-          prisma.hashtag({tag_name:args.tag_name}).posts({
-            first:args.limit,
-            skip: args.cur
-          })
+  Query: {
+    HashtagPost: async (_, args) =>
+      prisma.hashtags({
+        where: {
+          OR: [
+            { tag_name: args.tag_name }
+          ]
         }
-  };
-  
+      })
+  }
+};
